22e117f3e4bf51b45fa95d3c7748ae547baea0fb
lordforever <shashanktrivedi1917@gmail.com>
2025-06-11 00:03:05 +0530

    v0 final

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -105,6 +105,9 @@ const config: HardhatUserConfig = {
       // },
       loggingEnabled: false,
     },
+    localhost: {
+      accounts: ["0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"],
+    },
     anvil: {
       url: "http://127.0.0.1:8545",
       accounts: [deployerPrivateKey],
